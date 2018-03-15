04b524a6123f99973609e8c70abeaef35741a233
hyh <465855787@qq.com>
2018-03-15 13:19:56 +0800

    Calculator

--- calculator.py
+++ calculator.py
@@ -3,36 +3,42 @@
 import sys
 
 def insurence(money):
-	return money*0.08+money*0.02+money*0.005+money*0.06
+	m = money*0.08+money*0.02+money*0.005+money*0.06
+	return m
 
 def getTax(m):
-	money = None
-	if m<=1500:
-		money = format(m*0.03,".2f")
-	elif m>1500 and m<=4500:
-		money = format(m*0.1-105,".2f")
-	elif m>4500 and m<=9000:
-		money =format(m*0.2-555,".2f")
-	elif m>9000 and m<=35000:
-		money = format(m*0.25-1005,".2f")
-	elif m>35000 and m<=55000:
-		money = format(m*0.30-2755,".2f")
-	elif m>55000 and m<=80000:
-		money = format(m*0.35-5505,".2f")
-	else:
-		money = format(m*0.45-13505,".2f")
-	return money
 	
-#try:
+	if m<=1500:
+		return m*0.03
+	elif m>1500 and m<=4500:
+		return m*0.1-105
+	elif m>4500 and m<=9000:
+		return m*0.2-555
+	elif m>9000 and m<=35000:
+		return m*0.25-1005
+	elif m>35000 and m<=55000:
+		return  m*0.30-2755
+	elif m>55000 and m<=80000:
+		return m*0.35-5505
+	else:
+		return m*0.45-13505
+	
+	
+try:
 	for person in sys.argv[1:]:
 		number = int(person.split(":")[0])
 		money = int(person.split(":")[1])
-		m = money - insurence(money) -3500
-		m = money-getTax(m)
-		print('{}:{}'.format(number,m))
+		n = insurence(money)
+		if money<=3500:
+			print('{}:{}'.format(number,format(money-n,".2f")))
+		else:
+			m = money -n-3500
+			k = getTax(m)
+			new_money=money-n-k
+			print('{}:{}'.format(number,format(new_money,".2f")))
 
-#except ValueError:
-#	print("Parameter Error")
-#except TypeError:
-#	print("Parameter Error")
+except ValueError:
+	print("Parameter Error")
+except TypeError:
+	print("Parameter Error")
 
