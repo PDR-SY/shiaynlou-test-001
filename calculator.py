aa2ec28e39134f85d6991d17b1cbfc01659b76c3
hyh <465855787@qq.com>
2018-03-15 12:43:26 +0800

    Change Calculator

--- calculator.py
+++ calculator.py
@@ -1,26 +1,38 @@
 #! /usr/bin/env python3
 
 import sys
-try:
-	m = int(sys.argv[1])
-	m = m-3500
 
+def insurence(money):
+	return money*0.08+money*0.02+money*0.005+money*0.06
+
+def getTax(m):
+	money = None
 	if m<=1500:
-		print(format(m*0.03,".2f"))
+		money = format(m*0.03,".2f")
 	elif m>1500 and m<=4500:
-		print(format(m*0.1-105,".2f"))
+		money = format(m*0.1-105,".2f")
 	elif m>4500 and m<=9000:
-		print(format(m*0.2-555,".2f"))
+		money =format(m*0.2-555,".2f")
 	elif m>9000 and m<=35000:
-		print(format(m*0.25-1005,".2f"))
+		money = format(m*0.25-1005,".2f")
 	elif m>35000 and m<=55000:
-		print(format(m*0.30-2755,".2f"))
+		money = format(m*0.30-2755,".2f")
 	elif m>55000 and m<=80000:
-		print(format(m*0.35-5505,".2f"))
+		money = format(m*0.35-5505,".2f")
 	else:
-		print(format(m*0.45-13505,".2f"))
-except ValueError:
-	print("Parameter Error")
-except TypeError:
-	print("Parameter Error")
+		money = format(m*0.45-13505,".2f")
+	return money
+	
+#try:
+	for person in sys.argv[1:]:
+		number = int(person.split(":")[0])
+		money = int(person.split(":")[1])
+		m = money - insurence(money) -3500
+		m = money-getTax(m)
+		print('{}:{}'.format(number,m))
+
+#except ValueError:
+#	print("Parameter Error")
+#except TypeError:
+#	print("Parameter Error")
 
